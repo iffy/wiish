a0e11d62749a783f88b4ef3f3c3b39894d55dfc8
Matt Haggard <haggardii@gmail.com>
2018-10-25 22:46:14 -0600

    Basicmac example now uses library code to get the app started

--- examples/basicmac/basicmac.nim
+++ examples/basicmac/basicmac.nim
@@ -1,38 +1,7 @@
 ## Bare-bones MacOS application
-# import strutils
-# import sdl2
-# import sdl2/gfx
-# import system
-# discard sdl2.init(INIT_EVERYTHING)
+import wiishpkg/application
 
-when defined(macosx):
-  when defined(ios):
-    # iOS
-    echo "ios"
-  else:
-    # desktop macOS
-    proc someNimFunc {.exportc.} =
-      echo "some nim func"
-    
-    {.passL: "-framework Foundation" .}
-    {.passL: "-framework AppKit" .}
-    {.passL: "-framework ApplicationServices" .}
-    
-    {.compile: "stuff.m" .}
-    
-    type
-      Id {.importc: "id", header: "<objc/Object.h>", final.} = distinct int
-    
-    proc newWiish: Id {.importobjc: "Wiish new", nodecl .}
-    proc run(self: Id) {.importobjc: "run", nodecl .}
-    proc windowID(self: Id):Id {.importobjc: "windowID", nodecl .}
+app.ready.handle(message):
+  echo "App is ready"
 
-    proc displayWindow() =
-      var stuff:Id
-      echo "displaying window"
-      stuff = newWiish()
-      stuff.run
-      echo "after displaying window"
-
-if isMainModule:
-  displayWindow()
+app.start()
--- src/wiishpkg/application.nim
+++ src/wiishpkg/application.nim
@@ -1,11 +1,13 @@
+import ./events
+export events
+
 type
   App = object
-    on_ready_procs: seq[proc()]
+    ready*: EventSource[bool]
 
 ## The singleton application instance.
 var app* = App()
-
-
+app.ready = newEventSource[bool]()
 
 when defined(macosx):
   when defined(ios):
@@ -13,4 +15,82 @@ when defined(macosx):
     discard
   else:
     # macOS desktop
-    discard
+    proc mac_onReady {.exportc.} =
+      app.ready.emit(true)
+    
+    {.passL: "-framework Foundation" .}
+    {.passL: "-framework AppKit" .}
+    {.passL: "-framework ApplicationServices" .}
+    
+    {.emit: """
+#include <Cocoa/Cocoa.h>
+//// MyWindow
+//
+//@implementation MyWindow
+//- (BOOL) canBecomeKeyWindow
+//{
+//    return YES;
+//}
+//- (BOOL) canBecomeMainWindow
+//{
+//    return YES;
+//}
+//@end
+
+// AppDelegate
+@interface AppDelegate : NSObject <NSApplicationDelegate>{}
+@end
+@implementation AppDelegate
+- (BOOL) applicationShouldTerminateAfterLastWindowClosed: (NSNotification*) aNotification {
+    return YES;
+}
+- (void) applicationDidFinishLaunching:(NSNotification *)aNotification {
+  printf("Application finished launching\n");
+  mac_onReady();
+}
+- (void) applicationWillTerminate:(NSNotification *)aNotification {
+  printf("Application will terminate\n");
+}
+@end
+
+// Wiish
+@interface Wiish: NSObject
+{
+  // MyWindow* window;
+}
+- (void)run;
+@end
+@implementation Wiish
+- (void)run
+{
+  printf("init start\n");
+  [NSApplication sharedApplication];
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+  //NSUInteger windowStyle = NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask;
+  //NSRect windowRect = NSMakeRect(100,100,400,300);
+  //window = [[MyWindow alloc] 
+  //  initWithContentRect: windowRect
+  //  styleMask: windowStyle
+  //  backing: NSBackingStoreBuffered
+  //  defer: YES];
+  //[window setTitle: @"Hello, World!"];
+  //[window display];
+  //[window orderFrontRegardless];
+  //[window makeKeyWindow];
+  AppDelegate* appDel = [[AppDelegate alloc] init];
+  [NSApp setDelegate: appDel];
+  [NSApp run];
+}
+@end
+    """.}
+    
+    type
+      Id {.importc: "id", header: "<objc/Object.h>", final.} = distinct int
+    
+    proc newWiish: Id {.importobjc: "Wiish new", nodecl .}
+    proc run(self: Id) {.importobjc: "run", nodecl .}
+
+    proc start*(app:App) =
+      echo "Starting app"
+      var wiish = newWiish()
+      wiish.run()
