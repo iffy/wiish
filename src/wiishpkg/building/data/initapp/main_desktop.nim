581bbdaf705e6e5645995c1cb4fcd5a5fb31b726
Matt Haggard <haggardii@gmail.com>
2018-11-13 09:20:12 -0700

    Tests pass on local machine

--- src/wiishpkg/building/data/initapp/main_desktop.nim
+++ src/wiishpkg/building/data/initapp/main_desktop.nim
@@ -1,47 +1,48 @@
 ## Hello, World Wiish App
 import wiishpkg/desktop
-import sdl2, sdl2/gfx, sdl2/ttf
-import ospaths
+import sdl2/sdl except log
+# import sdl2/sdl_gfx
+import sdl2/sdl_ttf as ttf
 
 app.launched.handle:
   log "App launched"
-  ttfInit()
+  discard ttf.init()
 
   var w = app.newSDLWindow(title = "Hello, SDL Wiish!")
   var renderer = createRenderer(w.sdlWindow, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
   if renderer.isNil:
-    destroy w.sdlWindow
+    w.sdlWindow.destroyWindow()
     quit(1)
   
   # Open the font file
   let fontsize = (32.0).cint
-  let fontfile = currentSourcePath.parentDir/"Lato-Regular.ttf"
+  let fontfile = app.resourcePath("Lato-Regular.ttf")
   log "Trying to open font: ", fontfile.repr
-  let font:FontPtr = openFont(fontfile, fontsize)
-  var rectangle = rect(50, 50, 50, 50)
+  let font = openFont(fontfile, fontsize)
+  var rectangle = sdl.Rect(x: 50, y: 50, w: 50, h: 50)
 
   # Create the text
-  var textSurface:SurfacePtr = font.renderTextBlended("Hello, World!", color(50, 100, 50, 255))
-  var textRect = rect(20, 20, textSurface.w, textSurface.h)
-  var texture:TexturePtr = renderer.createTextureFromSurface(textSurface)
+  var textSurface = font.renderTextBlended("Hello, World!", tupleToColor((50, 100, 50, 255)))
+  var textRect = sdl.Rect(x: 20, y: 20, w: textSurface.w, h: textSurface.h)
+  var texture = renderer.createTextureFromSurface(textSurface)
   textSurface.freeSurface()
 
   # Perform drawing for the window.
   w.onDraw.handle(rect):
     log "onDraw"
     # Draw background
-    renderer.setDrawColor 255,255,255,255
-    renderer.clear
+    discard renderer.setRenderDrawColor(255,255,255,255)
+    discard renderer.renderClear()
 
     # Draw rectangle
-    renderer.setDrawColor 0,0,255,255
-    renderer.fillRect(rectangle.unsafeAddr)
+    discard renderer.setRenderDrawColor(0,0,255,255)
+    discard renderer.renderFillRect(rectangle.unsafeaddr)
 
     # Render the text
-    renderer.copy(texture, nil, textRect.unsafeAddr)
+    discard renderer.renderCopy(texture, nil, textRect.unsafeAddr)
 
     # Make it so!
-    renderer.present
+    renderer.renderPresent()
 
 app.willExit.handle:
   # Run this code just before the application exits
