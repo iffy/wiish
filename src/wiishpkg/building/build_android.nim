b0e678840515538ebb23c6ebcf6843abd7026a64
Matt Haggard <haggardii@gmail.com>
2018-11-13 17:08:42 -0700

    Closer on android

--- src/wiishpkg/building/build_android.nim
+++ src/wiishpkg/building/build_android.nim
@@ -1,9 +1,11 @@
 import os
 import osproc
+import re
 import ospaths
 import logging
 import strformat
 import strutils
+import tables
 import posix
 import parsetoml
 
@@ -16,29 +18,35 @@ proc getEnvOrFail(name: string, errmessage: string = ""): string =
   else:
     raise newException(CatchableError, &"Environment variable {name} must be set.  {errmessage}")
 
+proc replaceInFile(filename: string, replacements: Table[string, string]) =
+  ## Replace lines in a file with the given replacements
+  var guts = filename.readFile()
+  for pattern, replacement in replacements:
+    guts = guts.replace(re(pattern), replacement)
+  filename.writeFile(guts)
+
 proc doAndroidBuild*(directory:string, configPath:string): string =
   ## Package an Android app
   ## Returns the path to the app
+
+  # Following the SDL2 Android README in the SDL2 source
+  # look in SDL2SOURCE/docs/README-android.md
   let
     config = getAndroidConfig(configPath)
-    buildDir = directory/config.dst/"android"/"build"
-    buildPackageDir = buildDir/config.java_package_name
+    projectDir = directory/config.dst/"android"/"project"
     appSrc = directory/config.src
-    sdlSrc = DATADIR/"sdl2src"
-    androidNDKPath = getEnvOrFail("ANDROID_NDK", "Set to your local Android NDK path.  Download from https://developer.android.com/ndk/downloads/")
-    androidSDKPath = getEnvOrFail("ANDROID_SDK", "Set to your local Android SDK path.  Download from https://developer.android.com/studio/#downloads")
+    sdlSrc = DATADIR/"SDL"
+    # androidNDKPath = getEnvOrFail("ANDROID_NDK", "Set to your local Android NDK path.  Download from https://developer.android.com/ndk/downloads/")
+    # androidSDKPath = getEnvOrFail("ANDROID_SDK", "Set to your local Android SDK path.  Download from https://developer.android.com/studio/#downloads")
   var
     nimFlags: seq[string]
     ndkArgs: seq[string]
   
-  if not buildPackageDir.existsDir():
-    debug &"Copying SDL android project to {buildPackageDir}"
-    createDir(buildPackageDir)
-    copyDir(sdlSrc/"android-project", buildPackageDir)
-    createDir(buildPackageDir/"jni/SDL")
-    copyDir(sdlSrc/"src", buildPackageDir/"jni/SDL/src")
-    copyDir(sdlSrc/"include", buildPackageDir/"jni/SDL/include")
-    copyFile(sdlSrc/"Android.mk", buildPackageDir/"jni/SDL/Android.mk")
+  if not projectDir.existsDir():
+    debug &"Copying SDL android project to {projectDir}"
+    createDir(projectDir)
+    copyDirWithPermissions(sdlSrc/"android-project", projectDir)
+    copyDirWithPermissions(sdlSrc, projectDir/"app/jni/SDL")
 
   debug "Compiling Nim portion ..."
   nimFlags.add(["nim", "c"])
@@ -46,29 +54,35 @@ proc doAndroidBuild*(directory:string, configPath:string): string =
     "--os:linux",
     "-d:android",
     "--compileOnly",
-    "--cpu:arm",
+    # "--cpu:x86_64",
     "--dynlibOverride:SDL2",
     "--noMain",
-    "--warning[LockLevel]:off",
-    "--verbosity:0",
-    "--hint[Pattern]:off",
-    "--parallelBuild:0",
-    "--nimcache:" & buildPackageDir/"jni/src",
+    "--header",
+    "--nimcache:" & projectDir/"app/jni/src",
     appSrc,
-    ])
+  ])
   debug nimFlags.join(" ")
   run(nimFlags)
+
+  replaceInFile(projectDir/"app/jni/src/Android.mk", {
+    # XXX This is hard-coded to wiish_main_mobile.c right now, but should
+    # instead be based on the actual c code
+    "LOCAL_SRC_FILES.*?\n": "LOCAL_SRC_FILES := wiish_main_mobile.c\n",
+  }.toTable)
+
+  debug "Building with gradle ..."
+  run(projectDir/"gradlew", "installDebug")
   
-  debug "Doing NDK build ..."
-  ndkArgs.add(androidNDKPath/"ndk-build")
-  ndkArgs.add(@[
-    "--directory=" & buildPackageDir,
-    "V=1", # Verbose
-    "NDK_DEBUG=1", # Debug
-    "APP_OPTIM=debug", # Debug
-  ])
-  debug ndkArgs.join(" ")
-  run(ndkArgs)
+  # debug "Doing NDK build ..."
+  # ndkArgs.add(androidNDKPath/"ndk-build")
+  # ndkArgs.add(@[
+  #   "--directory=" & buildPackageDir,
+  #   "V=1", # Verbose
+  #   "NDK_DEBUG=1", # Debug
+  #   "APP_OPTIM=debug", # Debug
+  # ])
+  # debug ndkArgs.join(" ")
+  # run(ndkArgs)
 
   echo "Android IS NOT SUPPORTED YET"
 
