56d6b1c752b667e5f785f2bf44542c3cf3af606a
Matt Haggard <haggardii@gmail.com>
2018-11-21 11:32:36 -0700

    Build docs as part of travis tests

--- wiishpkg/events.nim
+++ wiishpkg/events.nim
@@ -1,3 +1,20 @@
+## This module implements ``EventSource``, which lets you 
+## register event handlers and then emit events.
+runnableExamples:
+  var
+    es = newEventSource[string]()
+    messages: seq[string]
+    count: int
+  es.handle(msg):
+    messages.add(msg)
+  es.handle:
+    inc(count)
+  es.emit("foo")
+  es.emit("bar")
+  doAssert messages == @["foo", "bar"]
+  doAssert count == 2
+
+
 import sequtils
 
 type
@@ -8,9 +25,12 @@ proc newEventSource*[T](): EventSource[T] =
   result = EventSource[T]()
 
 proc addListener*[T](es: var EventSource[T], listener: proc(message:T):void) =
+  ## Add a proc to handle events.  Proc will be called once for each
+  ## event emit.  See the ``handle`` template for a nicer syntax.
   es.listeners.add(listener)
 
 proc removeListener*[T](es: var EventSource[T], listener: proc(message:T):void) =
+  ## Remove a proc from receiving any more events.
   es.listeners.del(es.listeners.find(listener))
 
 proc emit*[T](es: EventSource[T], message: T) =
@@ -20,15 +40,21 @@ proc emit*[T](es: EventSource[T], message: T) =
 
 template handle*[T](es: EventSource[T], varname:untyped, fnbody:untyped): untyped =
   ## Convient syntax for addListener
-  ##
-  ## .. code-block :: nim
-  ##    eventsource.handle(message):
-  ##      echo "got the message: ", message
-  ##
+  runnableExamples:
+    var es = newEventSource[string]()
+    es.handle(thestring):
+      echo "Got thestring: " & thestring
+    es.emit("some string")
+
   es.addListener(proc(varname:T) =
     fnbody
   )
 
 template handle*[T](es: EventSource[T], fnbody: untyped): untyped =
   ## Convient syntax for when you don't care about the event's message
+  runnableExamples:
+    var es = newEventSource[string]()
+    es.handle:
+      echo "Got a message"
+    es.emit("some string")
   handle(es, _, fnbody)
