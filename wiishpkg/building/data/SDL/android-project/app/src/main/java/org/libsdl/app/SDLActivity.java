e3f77aecfb77a79bc4261ae3699adc77e78af169
Henrique Dias <mrhdias@gmail.com>
2019-04-11 14:49:12 +0100

    Fix for "Android error libsdl2.so failed to load"
    With this fix the app works on older android devices.
    More information:
    https://discourse.libsdl.org/t/android-error-libsdl2-so-failed-to-load/25680

--- wiishpkg/building/data/SDL/android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ wiishpkg/building/data/SDL/android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -142,6 +142,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
      */
     protected String[] getLibraries() {
         return new String[] {
+            "hidapi",
             "SDL2",
             // "SDL2_image",
             // "SDL2_mixer",
