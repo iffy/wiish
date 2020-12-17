6a0e3999a92492b5d3f53a2b00ec021cd0d82821
Matt Haggard <haggardii@gmail.com>
2020-12-17 15:49:00 -0500

    All iOS devices

--- .github/ios_devices.nim
+++ .github/ios_devices.nim
@@ -3,15 +3,11 @@ import strutils
 import osproc
 
 let output = execCmdEx("xcrun simctl list devices 'iphone 11' --json").output
-stderr.write(output)
 let data = output.parseJson()
 let devices = data["devices"]
-var ios_devices:JsonNode
 for k,v in devices.pairs():
   if ".iOS" in k:
-    ios_devices = v
-    break
-
-for item in ios_devices:
-  if item["isAvailable"].getBool(false):
-    echo item["udid"].getStr()
+    for item in v:
+      if item["isAvailable"].getBool(false):
+        echo item["udid"].getStr()
+  
