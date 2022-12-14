7627ccc12f2fe551f84c1356b261150178b537c1
Matt Haggard <haggardii@gmail.com>
2022-12-13 23:10:15 -0500

    Fix sdl2 example

--- examples/sdl2/wiish_build.nim
+++ examples/sdl2/wiish_build.nim
@@ -1,7 +1,26 @@
 import wiish/build
 import wiish/plugins/sdl2
 
+var wb = WiishBuild(
+  configure: proc(ctx: ref BuildContext) =
+    var cfg = ctx.config
+    cfg.name = if ctx.desktop: "Wiish SDL Demo" else: "WiishSDL"
+    cfg.version = "0.1.0"
+    cfg.src = if ctx.desktop: "main_desktop.nim" else: "main_mobile.nim"
+    cfg.outDir = "dist"
+    cfg.appWindowFormat = SDL
+    cfg.with(MacConfig, c):
+      c.bundle_id = "org.wiish.sdlexample"
+    cfg.with(MacDesktopConfig, c):
+      c.category_type = "public.app-category.example"
+    cfg.with(MaciOSConfig, c):
+      c.sdk_version = ""
+      c.provisioning_profile = ""
+    cfg.with(AndroidConfig, c):
+      c.java_package_name = "org.wiish.sdlexample"
+)
+
 build((
-  WiishBuild(),
+  wb,
   WiishSDL2Plugin(),
 ))
